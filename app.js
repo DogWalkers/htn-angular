08e8a47e95393a3aa63e7e05c177d3cad43c9708
Aditya Sridhar <aditya123ss@gmail.com>
2014-09-20 02:21:30 -0400

    angular routeprovider setup

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 angular
 	.module('app', [])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: './templates/main.html',
+                controller: 'appCtrl'
+                })
+            .otherwise({ redirectTo: '/' });
+    }])
     .controller('appCtrl', ['$scope', '$http', function($scope, $http){ 
     	console.log("in appCtrl");
         $scope.var = "abc";
