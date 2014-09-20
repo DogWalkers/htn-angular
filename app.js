14b288b7d49bc0aab02c436fe86031082da884bc
Aditya Sridhar <aditya123ss@gmail.com>
2014-09-20 04:11:04 -0400

    login page setup

--- app.js
+++ app.js
@@ -3,12 +3,40 @@ angular
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
             .when('/', {
-                templateUrl: './templates/main.html',
-                controller: 'appCtrl'
+                templateUrl: './templates/login.html',
+                controller: 'loginCtrl'
                 })
+            // .when('/login', {   //   /#/login
+            //     templateUrl: './templates/login.html',
+            //     controller: 'loginCtrl'
+            // })
             .otherwise({ redirectTo: '/' });
     }])
+
+
     .controller('appCtrl', ['$scope', '$http', function($scope, $http){ 
     	console.log("in appCtrl");
-        $scope.var = "abc";
+    }])
+
+    .controller('loginCtrl', ['$scope', '$http', function($scope, $http){
+        $scope.showSignIn = false;
+        $scope.showSignUp = false;
+        $scope.showClinicOrPatient = true;
+        
+        $scope.signInPatient = function(){
+            console.log("sign in patient");
+        }
+
+        $scope.signInClinic = function(){
+            console.log("signInClinic");
+        }
+
+        $scope.signUpPatient = function(){
+            console.log("signUpPatient");
+        }
+
+        $scope.signUpClinic = function(){
+            console.log("signUpClinic");
+        }
+
     }]);
