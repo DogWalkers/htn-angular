12e85e46ae02fba4a84de9cfd83f77288e83d051
Sahil Jain <sahil.jain@live.ca>
2014-09-21 00:34:19 -0400

    more api factory stuff

--- app.js
+++ app.js
@@ -1,54 +1,31 @@
 angular
 .module('app', ['ngCookies'])
-    //store to cookie
-    //Note: you need to change $window.location.href thing.
-    .controller('loginCtrl', ['$scope', '$http', '$window', '$cookieStore', '$location', function($scope, $http, $window, $cookieStore, $location){
-        $scope.isClinic = false;
-        $scope.isPatient = true;
-        $scope.showSignIn = false;
-        $scope.showSignUp = false;
-        $scope.showClinicOrPatient = true;
-        console.log("Into login controller");
-        //signin patients and clinics
+.controller('loginCtrl', ['$scope', '$http', '$window', '$cookieStore', '$location', 'API', function($scope, $http, $window, $cookieStore, $location, API){
+    $scope.isClinic = false;
+    $scope.isPatient = true;
+    $scope.showSignIn = false;
+    $scope.showSignUp = false;
+    $scope.showClinicOrPatient = true;
 
-        //log them in if they are already logged in.
-        if($cookieStore.get('token')) {
-            if ($cookieStore.get('token').patient) {
-                $location.path('/patientLoggedIn');
-            } else {
-                $location.path('/clinicLoggedIn');
-            }
-        } 
-        $scope.signIn = function(){
-            if($scope.isClinic == true){
-                $http({
-                    method: 'POST',
-                    url: 'http://hackthenorth-myfirstnodeapp.rhcloud.com/api/clinic/login',
-                    data: {
-                        ownerEmail: $scope.signInEmail,
-                        ownerPassword: $scope.signInPassword},
-                        headers: {'Content-Type': 'application/json'}
-                    }).success(function(data, status, headers, config){
-                        console.log(data);
-                        console.log("success");
-                        $cookieStore.put('token', {access_token: data.token, patient: false});
-                        location.path('/clinicLoggedIn');
-                    }).error(function(){
-                        alert("failed to log in");
-                    });
-                }
-                else{
-                    $http({
-                        method: 'POST',
-                        url: 'http://hackthenorth-myfirstnodeapp.rhcloud.com/api/patient/login',
-                        data: {
-                            email: $scope.signInEmail,
-                            password: $scope.signInPassword},
-                            headers: {'Content-Type': 'application/json'}
-                        }).success(function(data, status, headers, config){
-                            console.log(data);
-                            console.log("success");
-                            $cookieStore.put('token', {access_token: data.token, patient: true});
+    if($cookieStore.get('token')) {
+        if ($cookieStore.get('token').patient) {
+            $location.path('/patientLoggedIn');
+        } else {
+            $location.path('/clinicLoggedIn');
+        }
+    } 
+    $scope.signIn = function(){
+        if($scope.isClinic == true){
+            API.clinicLogin($scope.signInEmail, $scope.signInPassword).success(function(data, status, headers, config){
+                $cookieStore.put('token', {access_token: data.token, patient: false});
+                location.path('/clinicLoggedIn');
+            }).error(function(){
+                alert("failed to log in");
+            });
+        }
+        else{
+            API.patientLogin($cope.signInEmail, $scope.signInPassword).success(function(data, status, headers, config){
+                $cookieStore.put('token', {access_token: data.token, patient: true});
                         $location.path("/patientLoggedIn"); //You Need to change stuff here
                     }).error(function(){
                         alert("failed to log in");
@@ -57,54 +34,48 @@ angular
             }
 
             $scope.signUp = function(){
-            //signup clinic
-            if($scope.isClinic == true){
-                console.log("sign up as a clinic");
+                if($scope.isClinic == true){
+                    console.log("sign up as a clinic");
 
-                $http({
-                    method: 'POST',
-                    url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+$scope.clinicAddress+'&key=AIzaSyDW6_fagL8iR0nbdKa140dEKmiP4sC6D2k'
-                }).success(function(data, status, headers, config){
-                    console.log(data);
-                    console.log("success");
-                    if(data.results.length > 1){
-                        alert("Please enter a more precise location.");
-                    }  
-                    else if(data.results.length == 0){
-                        alert("You have entered an incorrect location.")
-                    }   
-                    else{
-                        $scope.formatted_address = data.results[0].formatted_address;
-                        $scope.clinicLatitude = data.results[0].geometry.location.lat;
-                        $scope.clinicLongitude = data.results[0].geometry.location.lng;
-                        console.log($scope.clinicLatitude);
-                        console.log($scope.clinicLongitude);
-                        $http({
-                            method: 'POST',
-                            url: 'http://hackthenorth-myfirstnodeapp.rhcloud.com/api/clinic/signup',
-                            data: {clinicName: $scope.clinicName,
-                                ownerEmail: $scope.clinicEmail,
-                                ownerPassword: $scope.clinicPassword,
-                                clinicAddress: $scope.clinicAddress,
-                                openTime: $scope.clinicOpenTime,
-                                closeTime: $scope.clinicCloseTime,
-                                clinicLatitude: $scope.clinicLatitude,
-                                clinicLongitude: $scope.clinicLongitude},
+                    var geocoder = new google.maps.Geocoder();
+                    var geocoderRequest = { address: $scope.clinicAddress };
+                    geocoder.geocode(geocoderRequest, function(results, status){
+                        console.log(results);
+                        if(results.length < 1) {
+                            alert("Could not find address!");
+                        } else if (results.length > 1) {
+                            alert("Please make your address more specific!");
+                        } else {
+                            var latitude = results[0].geometry.location.k;
+                            var longitude = results[0].geometry.location.B;
+                            console.log(latitude + "," + longitude);
+                            $scope.formatted_address = results[0].formatted_address;
+                            $scope.clinicLatitude = latitude;
+                            $scope.clinicLongitude = longitude;
+                            $http({
+                                method: 'POST',
+                                url: 'http://hackthenorth-myfirstnodeapp.rhcloud.com/api/clinic/signup',
+                                data: {
+                                    clinicName: $scope.clinicName,
+                                    ownerEmail: $scope.clinicEmail,
+                                    ownerPassword: $scope.clinicPassword,
+                                    clinicAddress: $scope.clinicAddress,
+                                    openTime: $scope.clinicOpenTime,
+                                    closeTime: $scope.clinicCloseTime,
+                                    clinicLatitude: $scope.clinicLatitude,
+                                    clinicLongitude: $scope.clinicLongitude
+                                },
                                 headers: {'Content-Type': 'application/json'}
                             }).success(function(data, status, headers, config){
                                 console.log(data);
                                 console.log("success");
                                 $cookieStore.put('token', {access_token: data.token, patient: false});
                                 location.path('/clinicLoggedIn');
-                    }).error(function(){
-                        alert("failed to sign up");
+                            });
+                        }   
                     });
-                }   
-            });
 
-}
-            //signup patients
-            else{
+                } else{
                 $http({
                     method: 'POST',
                     url: 'http://hackthenorth-myfirstnodeapp.rhcloud.com/api/patient/signup',
@@ -140,24 +111,24 @@ angular
                 //var markersArray = [];
                 var origin = new google.maps.LatLng(lat1, lng1);
                 var destination = new google.maps.LatLng(lat2, lng2);
-            
+
                 
-                  var service = new google.maps.DistanceMatrixService();
-                  service.getDistanceMatrix(
-                    {
-                      origins: [origin],
-                      destinations: [destination],
-                      travelMode: google.maps.TravelMode.DRIVING,
-                      unitSystem: google.maps.UnitSystem.METRIC,
-                      avoidHighways: false,
-                      avoidTolls: false
-                    }, callback);
+                var service = new google.maps.DistanceMatrixService();
+                service.getDistanceMatrix(
+                {
+                  origins: [origin],
+                  destinations: [destination],
+                  travelMode: google.maps.TravelMode.DRIVING,
+                  unitSystem: google.maps.UnitSystem.METRIC,
+                  avoidHighways: false,
+                  avoidTolls: false
+              }, callback);
                 
 
                 function callback(response, status) {
                   if (status != google.maps.DistanceMatrixStatus.OK) {
                     alert('Error was: ' + status);
-                  } else {
+                } else {
                     var origins = response.originAddresses;
                     var destinations = response.destinationAddresses;
 
@@ -172,37 +143,37 @@ angular
                       //       + ': ' + results[j].distance.text + ' in '
                       //       + results[j].duration.text + '<br>';
                       // }
-                    }
                   }
-                  return distanceInMeters;
-                }
-            }
+              }
+              return distanceInMeters;
+          }
+      }
 
 
-            $scope.getLatitudeLongitude = function(){
-                console.log($scope.patientAddress);
-                var patientAddressCompressed = $scope.patientAddress.replace(" ","+");
+      $scope.getLatitudeLongitude = function(){
+        console.log($scope.patientAddress);
+        var patientAddressCompressed = $scope.patientAddress.replace(" ","+");
 
 
-                $http({
-                    method: 'POST',
-                    url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+patientAddressCompressed+'&key=AIzaSyDW6_fagL8iR0nbdKa140dEKmiP4sC6D2k'
-                }).success(function(data, status, headers, config){
-                    console.log(data);
-                    console.log("success");
-                    if(data.results.length > 1){
-                        alert("Please enter a more precise location.");
-                    }  
-                    else if(data.results.length == 0){
-                        alert("You have entered an incorrect location.")
-                    }   
-                    else{
-                        $scope.formatted_address = data.results[0].formatted_address;
-                        $scope.latitude = data.results[0].geometry.location.lat;
-                        $scope.longitude = data.results[0].geometry.location.lng;
-                        console.log($scope.latitude);
-                        console.log($scope.longitude);
-                    }   
-                });
-            }
-        }]);
+        $http({
+            method: 'POST',
+            url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+patientAddressCompressed+'&key=AIzaSyDW6_fagL8iR0nbdKa140dEKmiP4sC6D2k'
+        }).success(function(data, status, headers, config){
+            console.log(data);
+            console.log("success");
+            if(data.results.length > 1){
+                alert("Please enter a more precise location.");
+            }  
+            else if(data.results.length == 0){
+                alert("You have entered an incorrect location.")
+            }   
+            else{
+                $scope.formatted_address = data.results[0].formatted_address;
+                $scope.latitude = data.results[0].geometry.location.lat;
+                $scope.longitude = data.results[0].geometry.location.lng;
+                console.log($scope.latitude);
+                console.log($scope.longitude);
+            }   
+        });
+    }
+}]);
