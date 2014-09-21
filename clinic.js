acdb55fbdcbce31da891bf2ca4d1ff3403ba2391
Aditya Sridhar <aditya123ss@gmail.com>
2014-09-21 08:28:17 -0400

    functionality for clinic view added

--- clinic.js
+++ clinic.js
@@ -1,4 +1,14 @@
 angular.module('app')
 	.controller('clinicCtrl', ['$scope', '$http', '$window', '$cookieStore', '$location', 'API', function($scope, $http, $window, $cookieStore, $location, API){
 		$scope.variable = "abc";
+		var token = $cookieStore.get('token');
+		$http({
+			method: "GET",
+			url: "http://hackthenorth-myfirstnodeapp.rhcloud.com/api/clinic?token="+token.access_token
+		}).success(function(data){
+			console.log("data is ready");
+			console.log(data);
+			$scope.patientsInQueue = data.patientsInQueue;
+			
+		});
 	}]);
