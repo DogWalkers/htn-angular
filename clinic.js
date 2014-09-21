2af62e9a78d56ff04a3fa9109ce7f635e2e74e51
Aditya Sridhar <aditya123ss@gmail.com>
2014-09-21 08:47:28 -0400

    added clinic name and clinic address

--- clinic.js
+++ clinic.js
@@ -8,7 +8,9 @@ angular.module('app')
 		}).success(function(data){
 			console.log("data is ready");
 			console.log(data);
+			$scope.clinicName = data.clinicName;
+			$scope.clinicAddress = data.clinicAddress;
 			$scope.patientsInQueue = data.patientsInQueue;
-			
+			$scope.patientsInQueue = [{patientId: 1, reasonForVisit: "2014-09-20T23:10:29.380Z", timeEntered: "2014-09-20T23:10:29.380Z"},{patientId: 1, reasonForVisit: "dsgbhjrgej", timeEntered: "shgvhj"},{patientId: 1, reasonForVisit: "dsgbhjrgej", timeEntered: "shgvhj"}]
 		});
 	}]);
